87dcf4e0aab0311003f94ad10a804b6250725d5a
mitchell demers <mitchelldemers66@gmail.com>
2016-09-28 11:15:20 -0400

    working cities jquery with refresh every on change event, also added a search all function

--- app/assets/javascripts/spaces.js
+++ app/assets/javascripts/spaces.js
@@ -12,10 +12,28 @@ $(function(){
     $('#city').change(function(event){
       var city = this.value;
       console.log(city) // return value is 'Toronto' , 'Hamilton' ..
-      $('.space-info').html("")
+
+      // clears the html in the space-info class
+      if (city != 'All'){
+        $('.space-info').hide();
+      }
+      else{
+        $('.space-info').show();
+      }
+
+      $('#search-results').html("")
+
+      //loops through all data
       for(var i = 0, l = _allInfo.length; i < l; i++){
+
+        console.log(_allInfo[i]['city']) //returns each city in the console
+
+        // if the chosen city is equal to the city in the list.. 
         if(_allInfo[i]['city'] === city){
-          $('<div>').html("name:" + _allInfo[i]['name']).attr('id', _allInfo[i]['id']).appendTo(".space-info")
+          console.log(_allInfo[i]['name'])
+          //append the new information into cleared space info class
+          $("<div>").html(_allInfo[i]['name']+ " ").attr('id', _allInfo[i]['id']).appendTo("#search-results")
+          $('<a>').attr('class', 'show-btn').attr('href', '/spaces/' + _allInfo[i]['id']).html('spaces show page').appendTo("#" + _allInfo[i]['id'])
         }
       }
     });
@@ -37,3 +55,8 @@ $(function(){
   });
 
 });
+
+
+
+// ment.style.visibility = 'hidden';      // Hide
+// element.style.visibility = 'visible';     // Show
