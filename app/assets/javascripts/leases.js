5674d067ee0539325c1a49f9b08ed295073aae46
Ian Bruce <brucegordonian@gmail.com>
2016-10-04 14:03:45 -0400

    fixed total price display

--- app/assets/javascripts/leases.js
+++ app/assets/javascripts/leases.js
@@ -4,10 +4,10 @@ $(function(){
 
   $('#new-lease-form').delegate("#lease_desks", "change", function(event){
     var desks = $(this).val();
-    console.log($(this).val());
+    // console.log($(this).val());
     var price = $('span#pricenumber').text();
-    console.log($('span#pricenumber').text())
-    $('.total-price').text(desks * price);
+    // console.log($('span#pricenumber').text())
+    $('span#total-price-value').text(desks * price + ".0");
 
    });
 
