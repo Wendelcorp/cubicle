8ca28fecc5fc12ec96ddaec70a9d788de3dbafab
Ian Bruce <brucegordonian@gmail.com>
2016-10-04 13:04:13 -0400

    fixed desks and total bug

--- app/assets/javascripts/leases.js
+++ app/assets/javascripts/leases.js
@@ -4,10 +4,11 @@ $(function(){
 
   $('#new-lease-form').delegate("#lease_desks", "change", function(event){
     var desks = $(this).val();
+    console.log($(this).val());
+    var price = $('span#pricenumber').text();
+    console.log($('span#pricenumber').text())
+    $('.total-price').text(desks * price);
 
-    var price = $('span#price').text();
-
-    $('.total-price').text(desks * price)
    });
 
   });
