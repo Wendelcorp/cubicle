16eb13ff488c443f5b8f8e9c1ac676fb11fd35ca
mitchell demers <mitchelldemers66@gmail.com>
2016-10-14 17:01:30 -0400

    testing front mouseenter functions

--- app/assets/javascripts/spaces.js
+++ app/assets/javascripts/spaces.js
@@ -21,7 +21,6 @@ var querystring
       dataType: 'json'
     }).done(function(data){
       var _allData = data
-      console.log(_allData)
       // globalish variables for all data and empty array for available data
       var _availableData = []
       var city = 'all';
@@ -29,20 +28,20 @@ var querystring
       console.log(_allData)
 
       function populate(i) {
-        $("<div>").attr('id', _allData[i]['id']).attr('class', 'space-box').appendTo('.space-info')
-        $('<p>').attr('class', 'space-price').html('$' + Number(_allData[i]['price']).toFixed(2)).appendTo("#" + _allData[i]['id'])
-        $('<a>').attr('class', 'show-btn').attr('href', '/spaces/' + _allData[i]['id']).attr('id', 'link' + _allData[i]['id']).appendTo("#" + _allData[i]['id'])
+        $("<div>").attr('id', i).attr('class', 'space-box').appendTo('.space-info') // .html('_allData[i]['name']')
+        $('<p>').attr('class', 'space-price').html('$' + Number(_allData[i]['price']).toFixed(2)).appendTo("#" + i)
+        $('<a>').attr('class', 'show-btn').attr('href', '/spaces/' + _allData[i]['id']).attr('id', 'link' + _allData[i]['id']).appendTo("#" + i)
         $('<img>').attr('class','front-page-img').attr('src',  imgHash[parseInt(_allData[i]['id'])]).appendTo('#link' + _allData[i]['id'])
-        $('#link' + _allData[i]['id']).wrap( "<div class='front-page-img-container' id = '"+ i + "' ></div>");
+        $('#link' + _allData[i]['id']).wrap( "<div class='front-page-img-container' id = '" + (i) + "container' ></div>");
       }
       function hoverOnAndOff(){
         $('.front-page-img-container').mouseenter(function(event){
-        value = parseInt(this.id);
+        value = parseInt(this.id); // starts at 1
           console.log(value)
           $(this).stop().animate({opacity:.5},200);
-          $('<div>').html(_allData[value]['name']).attr('class','name').css("position", "absolute").css("top", "50px").css('font-weight', 'bold').appendTo('#'+value)
+          $('<div>').html(_allData[value]['name']).attr('class','name').css("position", "absolute").css("top", "50px").css('font-weight', 'bold').appendTo('#'+(value))
           $('<div>').html("Available Desks: " + _allData[value]['available_desks']).attr("class",'available_desks').css("position", "absolute")
-          .css("top", "65px").css('font-weight', 'bold').appendTo('#'+value)
+          .css("top", "65px").css('font-weight', 'bold').appendTo('#'+(value))
       });
         $('.front-page-img-container').mouseleave(function(event){
           $('.available_desks').remove()
@@ -75,7 +74,9 @@ var querystring
 
 
             // if the chosen city is equal to the city selected in the list and desks is not changed
-
+            console.log(desks)
+            console.log(city)
+            console.log(dataCity)
 
             if(dataCity === city && desks === 1 ) {
               console.log('fuck ya')
@@ -136,7 +137,7 @@ var querystring
 
       if($('div').is('.index-page')){
         localStorage.setItem('desks', 1);
-      };
+      };//parseFloat(Math.round((loadDesks * price) * 100) / 100).toFixed(2);
 
       if($('span').is('#total-price-value')){
         var loadDesks = localStorage.desks
