a7864dff247c3240d3ea6e0e62b36bbcd8077c67
clark chen <chenhuajie1@gmail.com>
2016-10-17 18:32:43 -0400

    some mailer changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,26 +25,25 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["gmail_domain"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["gmail_username"],
+    password: ENV["gmail_password"]
   }
 
 
