1529f9415c73f5ff26275ecb9bde8a3c99cbed97
clark chen <chenhuajie1@gmail.com>
2016-10-17 22:13:46 -0400

    fix actioncable bug

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -1,23 +1,24 @@
 $(document).on("turbolinks:load", function () {
-  App.appearance = App.cable.subscriptions.create({
-    channel: 'AppearanceChannel'
-  }, {
-    disconnected: function(){},
-    received: function(data){
-      var userdata = JSON.parse(data);
+  if (App['appearance'] == undefined){
+    App['appearance'] = App.cable.subscriptions.create({
+      channel: 'AppearanceChannel'
+    }, {
+      disconnected: function(){},
+      received: function(data){
+        var userdata = JSON.parse(data);
 
-      var id = userdata.id;
-      var online = userdata.online;
+        var id = userdata.id;
+        var online = userdata.online;
 
-      if(online == true){
-        $(userImgIdConstructor(id)).attr('class','active');
-      };
-      if(online == false){
-        $(userImgIdConstructor(id)).attr('class','inactive');
-      };
-    }
-  });
-
+        if(online == true){
+          $(userImgIdConstructor(id)).attr('class','active');
+        };
+        if(online == false){
+          $(userImgIdConstructor(id)).attr('class','inactive');
+        };
+      }
+    });
+  }
   var userImgIdConstructor = function(id){
     return "#" + id + "-status";
   }
--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,11 +1,13 @@
 $(document).on("turbolinks:load", function () {
-  App.room = App.cable.subscriptions.create({
-    channel: 'RoomChannel',
-    room: window.location.pathname.split("/")[2]
-  },{
-    received: function(data){
-      $('.messages').append(data.message);
-      $('#newmessage').attr('class', 'active');
-    }
-  });
+  if (App[window.location.pathname.split("/")[2]] == undefined){
+    App[window.location.pathname.split("/")[2]] = App.cable.subscriptions.create({
+      channel: 'RoomChannel',
+      room: window.location.pathname.split("/")[2]
+    },{
+      received: function(data){
+        $('.messages').append(data.message);
+        $('#newmessage').attr('class', 'active');
+      }
+    });
+  }
 });
